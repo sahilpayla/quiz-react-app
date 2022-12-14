5803d46ad40a15ff234409c9a72c49445778ab73
sahilpayla <1shraddhanand@gmail.com>
2022-12-14 11:18:23 +0530

    quiz-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QuizComp from './quizCompnonents/QuizComp';
+
+function App() {
+  return (
+    <div className="App">
+     <QuizComp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/quizCompnonents/QuizComp.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+
+const QuizComp = () => {
+
+    const QuestionBank = [
+        {
+            Question: "Cartoon character Spongebob Squarepants lives in a what under the sea ?",
+            AnswerText: [
+                { Answer: 'PineApple', isCorrect: true },
+                { Answer: 'Tower', isCorrect: false },
+                { Answer: 'Castle', isCorrect: false },
+                { Answer: 'Grape', isCorrect: false }
+            ]
+        },
+        {
+            Question: "What does the character Popeye famously eat to boost his strength ?",
+            AnswerText: [
+                { Answer: 'Egg', isCorrect: false },
+                { Answer: 'Honey', isCorrect: false },
+                { Answer: 'Spinach', isCorrect: true },
+                { Answer: 'Cake', isCorrect: false }
+            ]
+        },
+        {
+            Question: "In which series will you find the cartoon characters Blossom, Bubbles and Buttercup ?",
+            AnswerText: [
+                { Answer: 'Oswald', isCorrect: false },
+                { Answer: 'Power Puff Girls', isCorrect: true },
+                { Answer: 'Bob The Builder', isCorrect: false },
+                { Answer: 'Noddy', isCorrect: false }
+            ]
+        },
+        {
+            Question: "The Cartoon Network channel was launched in the USA in which year ?",
+            AnswerText: [
+                { Answer: '2001', isCorrect: false },
+                { Answer: '1992', isCorrect: true },
+                { Answer: '1947', isCorrect: false },
+                { Answer: '1998', isCorrect: false }
+            ]
+        },
+        {
+            Question: "What is the thing which have so many holes ?",
+            AnswerText: [
+                { Answer: 'Oswald', isCorrect: false },
+                { Answer: 'Bob The Builder', isCorrect: false },
+                { Answer: 'Sponge Bob Pants', isCorrect: true },
+                { Answer: 'Noddy', isCorrect: false }
+            ]
+        },
+    ]
+
+    console.log(QuestionBank)
+    return (
+        <div>
+            quiz
+        </div>
+    )
+}
+
+export default QuizComp
